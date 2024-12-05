9e66527419a95e4dec5ba62d1d649e65316b1d65
Thach Vu <thachvu1127@gmail.com>
2024-12-05 11:46:08 -0800

    Finish form, add button, modified js to display error

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const paragraph = document.getElementById("error");
-const password = document.getElementById("password").value;
-while (password[0] === null) {
-    paragraph.textContent = "*Password Required*"
-}
+document.addEventListener("DOMContentLoaded", () => {
+
+    let password = document.getElementById("password");
+    let cPassword = document.getElementById("cPassword");
+    let error = document.getElementById("error");
+
+
+
+    if (password == "" && cPassword == "") {
+        error.textContent = "*Password Required";
+    } else if (password != cPassword) {
+        error.textContent = "*Password do not match";
+    } else {
+        error.textContent = "";
+    }
+
+})
